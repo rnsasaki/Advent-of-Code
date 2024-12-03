56da1aa889379dcf112c05f3a5f14f7a6f372d1b
Nabil Chafiai <97534636+rnsasaki@users.noreply.github.com>
2024-12-03 08:09:55 +0200

    Advent of code - 2023 commit - Day 1 included

---
+++ Advent of Code 2023/Day 1/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿using ElfSoulution;
+
+Calibration fileContent = new Calibration();
+
+fileContent.CalibrationValueCalculation("C:\\Users\\lazyb\\source\\repos\\Elf Solution\\ConsoleApp1\\input.txt");
+
+
+
+
